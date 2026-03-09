d39f4783bc8cef17fe268852562721f5d29f1388
joseph <josem.c7@icloud.com>
2026-03-09 17:35:14 -0400

    LibraryItemClass

--- src/main/java/com/zipcodewilmington/centrallibrary/LibraryItem.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/LibraryItem.java
@@ -11,6 +11,48 @@ public abstract class LibraryItem {
         this.title = title;
         this.location = location;
         this.isAvailable = true;
+
+        
     }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void checkOut() {
+        this.isAvailable = false;
+    }
+
+    public void checkIn() {
+        this.isAvailable = true;
+
+    } 
+
+    public abstract double calculateLateFee(int daysLate);
+    public abstract int getMaxBorrowDays();
+    public abstract String getItemType();
 
 }
