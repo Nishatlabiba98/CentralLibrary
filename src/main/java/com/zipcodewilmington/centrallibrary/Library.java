93a09097d7920f5fd83c234255497a94c5153c2c
james <jamesstrine@gmail.com>
2026-03-11 10:55:16 -0400

    Checkpoint on Library.java file

--- src/main/java/com/zipcodewilmington/centrallibrary/Library.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/Library.java
@@ -1,16 +1,50 @@
 package com.zipcodewilmington.centrallibrary;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Library {
+public abstract class Library {
+    private String name;
+    private String address;
+    private List<LibraryItem> items;
+    private List<LibraryMember> members;
+    private List<Librarian> librarians;
     
-    public static String name(String libraryNameString) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(libraryNameString);
-        String userInput = scanner.nextLine();
-        return userInput;
+    
+    public Library(String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.items = new ArrayList<>();
+        this.members = new ArrayList<>();
+        this.librarians = new ArrayList<>();
     }
 
-    
+    public String getname() {
+        return name;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public List<LibraryItem> getItems() {
+        return items;
+    }
+    public List<LibraryMember> getMembers() {
+        return members;
+    }
+    public List<Librarian> getLibrarians() {
+        return librarians;
+    }
 
+    public void addItem(LibraryItem item) {
+        items.add(item);
+    }
+    public void removeItem(LibraryItem item) {
+        items.remove(item);
+    }
+    public void addMember(LibraryMember member) {
+        members.add(member);
+    }
+    public void addLibrarian(Librarian librarian) {
+        librarians.add(librarian);
+    }
 }
